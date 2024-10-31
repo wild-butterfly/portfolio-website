5b6e7de48b340a0c63952718b8a61003fc369e1d
wild-butterfly <askinyagmurkose@outlook.com>
2024-10-31 20:46:44 +1100

    Update commit

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
-import './App.css';
 import Projects from './components/Projects';
+import './App.css';
 
 function App() {
   return (
